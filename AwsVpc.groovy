a7e4af7e91a8d4c95f2aef76b5a6de39617ffcd5
balogunrash <balogunrash@bastion-fuchicorp-com.us-central1-a.c.geometric-sled-282503.internal>
2020-10-29 02:40:17 +0000

    aws vpc groovy file created

---
+++ AwsVpc.groovy
@@ -0,0 +1,119 @@
+def k8slabel = "jenkins-pipeline-${UUID.randomUUID().toString()}"
+def slavePodTemplate = """
+      metadata:
+        labels:
+          k8s-label: ${k8slabel}
+        annotations:
+          jenkinsjoblabel: ${env.JOB_NAME}-${env.BUILD_NUMBER}
+      spec:
+        affinity:
+          podAntiAffinity:
+            requiredDuringSchedulingIgnoredDuringExecution:
+            - labelSelector:
+                matchExpressions:
+                - key: component
+                  operator: In
+                  values:
+                  - jenkins-jenkins-master
+              topologyKey: "kubernetes.io/hostname"
+        containers:
+        - name: docker
+          image: docker:latest
+          imagePullPolicy: Always
+          command:
+          - cat
+          tty: true
+          volumeMounts:
+            - mountPath: /var/run/docker.sock
+              name: docker-sock
+            - mountPath: /etc/secrets/service-account/
+              name: google-service-account
+        - name: fuchicorptools
+          image: fuchicorp/buildtools
+          imagePullPolicy: Always
+          command:
+          - cat
+          tty: true
+          volumeMounts:
+            - mountPath: /var/run/docker.sock
+              name: docker-sock
+            - mountPath: /etc/secrets/service-account/
+              name: google-service-account
+        serviceAccountName: common-service-account
+        securityContext:
+          runAsUser: 0
+          fsGroup: 0
+        volumes:
+          - name: google-service-account
+            secret:
+              secretName: google-service-account
+          - name: docker-sock
+            hostPath:
+              path: /var/run/docker.sock
+    """
+    
+properties([
+    parameters([
+        booleanParam(defaultValue: true, description: 'Do you want to run terrform apply', name: 'terraform_apply'),
+        booleanParam(defaultValue: false, description: 'Do you want to run terrform destroy', name: 'terraform_destroy'),
+        choice(choices: ['dev', 'qa', 'prod'], description: '', name: 'environment')
+    ])
+])
+
+def aws_region_var = ''
+
+if(params.environment == "dev"){
+    aws_region_var = "us-east-1"
+}
+else if(params.environment == "qa"){
+    aws_region_var = "us-east-2"
+}
+else if(params.environment == "prod"){
+    aws_region_var = "us-west-2"
+}
+
+podTemplate(name: k8slabel, label: k8slabel, yaml: slavePodTemplate) {
+    node(k8slabel) {
+        container('fuchicorptools') {
+
+
+    stage("Pull Repo"){
+        cleanWs()
+        git url: 'https://github.com/balloray/terraform-vpc.git'
+    }
+
+    withCredentials([usernamePassword(credentialsId: 'jenkins-aws-access-key', passwordVariable: 'AWS_SECRET_ACCESS_KEY', usernameVariable: 'AWS_ACCESS_KEY_ID')]) {
+        withEnv(["AWS_REGION=${aws_region_var}"]) {
+            stage("Terrraform Init"){
+                sh """
+                    bash setenv.sh ${environment}.tfvars
+                    terraform-0.13 init
+                """
+            }        
+            
+            if (terraform_apply.toBoolean()) {
+                stage("Terraform Apply"){
+                    sh """
+                        terraform-0.13 apply -var-file ${environment}.tfvars -auto-approve
+                    """
+                }
+            }
+            else if (terraform_destroy.toBoolean()) {
+                stage("Terraform Destroy"){
+                    sh """
+                        terraform-0.13 destroy -var-file ${environment}.tfvars -auto-approve
+                    """
+                }
+            }
+            else {
+                stage("Terraform Plan"){
+                    sh """
+                        terraform-0.13 plan -var-file ${environment}.tfvars
+                    """
+                }
+            }
+        }        
+    }    
+}
+}
+}
