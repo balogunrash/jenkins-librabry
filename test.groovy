c770672b31528ae1b8960c8ac812a3cdc26200ff
balogunrash <balogunrash@bastion-fuchicorp-com.us-central1-a.c.geometric-sled-282503.internal>
2020-10-29 02:19:44 +0000

    test file

---
+++ test.groovy
@@ -0,0 +1,69 @@
+//There is a need to create Credentials with type "secret file" for tfvars file with ID "common-tools"
+//There is a need to create Credentials with type "secret file" for ServiceAccount file with ID "common_json"
+
+properties([[$class: 'RebuildSettings', autoRebuild: false, rebuildDisabled: false], pipelineTriggers([cron('H 1 * * *')])])
+
+def k8slabel = "jenkins-pipeline-${UUID.randomUUID().toString()}"
+def slavePodTemplate = """
+      metadata:
+        labels:
+          k8s-label: ${k8slabel}
+        annotations:
+          jenkinsjoblabel: ${env.JOB_NAME}-${env.BUILD_NUMBER}
+      spec:
+        affinity:
+          podAntiAffinity:
+            requiredDuringSchedulingIgnoredDuringExecution:
+            - labelSelector:
+                matchExpressions:
+                - key: component
+                  operator: In
+                  values:
+                  - jenkins-jenkins-master
+              topologyKey: "kubernetes.io/hostname"
+        containers:
+        - name: docker
+          image: docker:latest
+          imagePullPolicy: Always
+          command:
+          - cat
+          tty: true
+          volumeMounts:
+            - mountPath: /var/run/docker.sock
+              name: docker-sock
+            - mountPath: /etc/secrets/service-account/
+              name: google-service-account
+        - name: fuchicorptools
+          image: fuchicorp/buildtools
+          imagePullPolicy: Always
+          command:
+          - cat
+          tty: true
+          volumeMounts:
+            - mountPath: /var/run/docker.sock
+              name: docker-sock
+            - mountPath: /etc/secrets/service-account/
+              name: google-service-account
+        serviceAccountName: common-service-account
+        securityContext:
+          runAsUser: 0
+          fsGroup: 0
+        volumes:
+          - name: google-service-account
+            secret:
+              secretName: google-service-account
+          - name: docker-sock
+            hostPath:
+              path: /var/run/docker.sock
+    """
+    
+    podTemplate(name: k8slabel, label: k8slabel, yaml: slavePodTemplate) {
+      node(k8slabel) {
+        stage("Docker check") {
+          container('fuchicorptools') {
+            sh 'docker --version'
+          }
+        }
+      }
+    }
+        
